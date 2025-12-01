af6d34e5a6bf9d737a8830aad081b239ddd20c42
titus-79 <rodolpheleg79@gmail.com>
2025-12-01 16:21:05 +0100

    feat: add Hand class with Blackjack scoring logic

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import models.Carte;
 import models.Couleur;
+import models.Hand;
 import models.Paquet;
 import models.Valeur;
 
@@ -19,17 +20,42 @@ public class Main {
         // System.out.println(carte2 + " vaut " + carte2.getValue());
         // System.out.println(carte3 + " vaut " + carte3.getValue());
 
-        Paquet paquet = new Paquet();
-        System.out.println("Cartes dans le paquet : " + paquet.getNombreCartesRestantes());
+        // Paquet paquet = new Paquet();
+        // System.out.println("Cartes dans le paquet : " + paquet.getNombreCartesRestantes());
 
-        paquet.melanger();
-        System.out.println("\nDistribution de 53 cartes :");
+        // paquet.melanger();
+        // System.out.println("\nDistribution de 53 cartes :");
         
-        for (int i = 1; i <= 53; i++) {
-            Carte carte = paquet.distribuerCarte();
-            System.out.println("Carte " + i + " : " + carte + " (valeur: " + carte.getValue() + ")");
-        }
+        // for (int i = 1; i <= 53; i++) {
+        //     Carte carte = paquet.distribuerCarte();
+        //     System.out.println("Carte " + i + " : " + carte + " (valeur: " + carte.getValue() + ")");
+        // }
         
-        System.out.println("\nCartes restantes : " + paquet.getNombreCartesRestantes());
+        // System.out.println("\nCartes restantes : " + paquet.getNombreCartesRestantes());
+        
+        Hand main = new Hand();
+        
+        // Test 1 : main normale
+        main.ajouterCarte(new Carte(Couleur.PIQUE, Valeur.AS));
+        main.ajouterCarte(new Carte(Couleur.COEUR, Valeur.NEUF));
+        System.out.println(main);
+        System.out.println("Score : " + main.getScore());  // Doit afficher 20
+        
+        System.out.println("\n---");
+        
+        // Test 2 : main avec As qui doit s'adapter
+        main.ajouterCarte(new Carte(Couleur.CARREAU, Valeur.CINQ));
+        System.out.println(main);
+        System.out.println("Score : " + main.getScore());  // Doit afficher 15 (As vaut 1)
+        
+        System.out.println("\n---");
+        
+        // Test 3 : nouvelle main
+        main.vider();
+        main.ajouterCarte(new Carte(Couleur.TREFLE, Valeur.ROI));
+        main.ajouterCarte(new Carte(Couleur.COEUR, Valeur.DIX));
+        System.out.println(main);
+        System.out.println("Score : " + main.getScore());  // Doit afficher 20
+
     }
 }
---
+++ src/models/Hand.java
@@ -0,0 +1,49 @@
+package models;
+
+import java.util.ArrayList;
+
+public class Hand {
+    
+    private ArrayList<Carte> hand = new ArrayList<>();
+
+    public Hand() {
+    }
+    
+    public void ajouterCarte(Carte carte) {
+        hand.add(carte);
+    }
+    
+    public int getScore() {
+        int score = 0;
+        int nbAs = 0;
+
+        for (Carte carte : hand) {
+            score += carte.getValue();
+            if (carte.getValeur() == Valeur.AS) {
+                nbAs++;
+            }
+        }
+
+        while (score > 21 && nbAs > 0) {
+            score -= 10;
+            nbAs--;
+        }
+        return score;
+    }
+    
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i< hand.size(); i++) {
+           sb.append(hand.get(i));
+           if (i < hand.size() - 1) {
+            sb.append(", ");
+           }
+        }
+        return sb.toString();
+    }
+    
+    public void vider() {
+        hand.clear();
+    }
+}
